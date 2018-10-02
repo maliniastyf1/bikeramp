6152cbf04b5a3135f512d0ef1b793c7f7d53dcaa
maliniastyf1 <adam.malinowski@netguru.pl>
2018-10-02 20:37:57 +0200

    Add attribute presence validation to trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 class Trip < ApplicationRecord
+  validates :start_address, presence: true
+  validates :destination_address, presence: true
+  validates :price, presence: true
+  validates :date, presence: true
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Trip do
@@ -7,5 +9,12 @@ RSpec.describe Trip do
     it { is_expected.to have_db_column(:price).of_type(:decimal) }
     it { is_expected.to have_db_column(:distance).of_type(:decimal) }
     it { is_expected.to have_db_column(:date).of_type(:datetime) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:start_address) }
+    it { is_expected.to validate_presence_of(:destination_address) }
+    it { is_expected.to validate_presence_of(:price) }
+    it { is_expected.to validate_presence_of(:date) }
   end
 end
