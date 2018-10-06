c1035af431b8d289d20b52255116fc6aea9b64fa
maliniastyf1 <adam.malinowski@netguru.pl>
2018-10-06 16:03:21 +0200

    Add endpoint with monthly reports

---
+++ app/services/stats_services/prepare_monthly_stats_report.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module StatsServices
+  class PrepareMonthlyStatsReport < ApplicationService
+    include Dry::Monads::Do.for(:call)
+
+    def call
+    end
+  end
+end
