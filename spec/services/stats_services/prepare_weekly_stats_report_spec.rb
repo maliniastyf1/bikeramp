d4df156b41630972507decdb2bf787168a7e3746
maliniastyf1 <adam.malinowski@netguru.pl>
2018-10-06 14:06:59 +0200

    Add service to prepare weekly rides report

---
+++ spec/services/stats_services/prepare_weekly_stats_report_spec.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe StatsServices::PrepareWeeklyStatsReport do
+  let(:date) { Date.new(2018, 10, 3) }
+  let(:date_2) { Date.new(2018, 10, 5) }
+  let(:date_out_of_range) { Date.new(2018, 9, 25) }
+
+  let!(:trip_1) do
+    create(
+      :trip,
+      destination_address: 'Sokratesa 13, Warszawa, Polska',
+      date: date,
+    )
+  end
+  let!(:trip_2) do
+    create(
+      :trip,
+      destination_address: 'Sokratesa 13, Warszawa, Polska',
+      date: date,
+    )
+  end
+  let!(:trip_3) do
+    create(
+      :trip,
+      destination_address: 'Sokratesa 13, Warszawa, Polska',
+      date: date_2,
+    )
+  end
+  let!(:trip_4) do
+    create(
+      :trip,
+      destination_address: 'Sokratesa 13, Warszawa, Polska',
+      date: date_out_of_range,
+    )
+  end
+
+  before do
+    Timecop.freeze(Time.zone.local(2018, 10, 6))
+  end
+
+  after do
+    Timecop.return
+  end
+
+  subject { described_class.call }
+
+  context 'when trips are in week range' do
+    it 'return success object' do
+      expect(subject.success?).to eq(true)
+    end
+
+    it 'return weekly stats' do
+      expect(subject.success[:total_distance]).to eq('60km')
+      expect(subject.success[:total_price]).to eq('150.00PLN')
+    end
+  end
+
+  context 'when trips are out of week range' do
+    let(:date) { Date.new(2018, 9, 3) }
+    let(:date_2) { Date.new(2018, 9, 5) }
+
+    it 'return success object' do
+      expect(subject.success?).to eq(true)
+    end
+
+    it 'return weekly stats' do
+      expect(subject.success[:total_distance]).to eq('0km')
+      expect(subject.success[:total_price]).to eq('0.00PLN')
+    end
+  end
+end
