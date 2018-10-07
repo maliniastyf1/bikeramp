251e042c8782f6b2eac9d9b7875e5de27f121efb
maliniastyf1 <adam.malinowski@netguru.pl>
2018-10-07 13:44:24 +0200

    Add scope to trip model to get trips grouped by day in range

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -5,4 +5,6 @@ class Trip < ApplicationRecord
   validates :destination_address, presence: true
   validates :price, presence: true
   validates :date, presence: true
+
+  scope :in_range_grouped_by_day, ->(start_date, end_date) { where('date >= ? AND date <= ?', start_date, end_date).group_by(&:date) }
 end
