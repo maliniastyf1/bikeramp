bf8cdb099da5edd78b6aff5e52381f4e894d6c17
maliniastyf1 <adam.malinowski@netguru.pl>
2018-10-07 13:48:02 +0200

    Fix styling errors from rubocop

--- spec/api/bikeramp/stats/monthly_spec.rb
+++ spec/api/bikeramp/stats/monthly_spec.rb
@@ -17,16 +17,16 @@ describe Bikeramp::Stats::Monthly, type: :request do
     let(:monthly_stats) do
       [
         {
-          "day":            "July, 4th",
-          "total_distance": "12km",
-          "avg_ride":       "4km",
-          "avg_price":      "22.75PLN"
+          'day':            'July, 4th',
+          'total_distance': '12km',
+          'avg_ride':       '4km',
+          'avg_price':      '22.75PLN'
         },
         {
-          "day":            "July, 5th",
-          "total_distance": "3km",
-          "avg_ride":       "3km",
-          "avg_price":      "15.5PLN"
+          'day':            'July, 5th',
+          'total_distance': '3km',
+          'avg_ride':       '3km',
+          'avg_price':      '15.5PLN'
         }
       ]
     end
